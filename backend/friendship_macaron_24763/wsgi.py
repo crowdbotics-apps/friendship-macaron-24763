1e6b47e0d24986f2fbef46c00ab15fdf8913e89a
Crowdbotics <team@crowdbotics.com>
2021-02-28 20:12:29 +0000

    Update models

--- backend/friendship_macaron_24763/wsgi.py
+++ backend/friendship_macaron_24763/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friendship_macaron_24763.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friendship_macaron_24763.settings")
 
 application = get_wsgi_application()
